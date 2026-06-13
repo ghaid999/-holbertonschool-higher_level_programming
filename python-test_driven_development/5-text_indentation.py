ea56c44d53a7dbc155e104595a6f74e427bb8494
ghaid999 <gaid.909@gmail.com>
2026-06-13 23:49:32 +0300

    Create 5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module that contains text_indentation function"""
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines after '.', '?' and ':'"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    skip_space = False
+
+    while i < len(text):
+        char = text[i]
+
+        if char in ".?:":
+            print(char)
+            print()
+            skip_space = True
+
+        elif char == " " and skip_space:
+            pass
+
+        else:
+            if skip_space:
+                skip_space = False
+            print(char, end="")
+
+        i += 1
+
+    print()
