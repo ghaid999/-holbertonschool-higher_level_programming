0e387471ac884dcdaf31d38efdd9296369fb280d
ghaid999 <gaid.909@gmail.com>
2026-06-24 20:37:10 +0300

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module that defines Square"""
 
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -10,9 +10,13 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialize square"""
         self.integer_validator("size", size)
-
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """Return square area"""
-        return self.__size* self.__size
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return square description"""
+        return f"[Square] {self.__size}/{self.__size}"
